446063d8052ec4c1974a9ccb67d41adf3b927a8e
Malik <malikmukhtar63@gmail.com>
2020-04-08 18:19:39 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -2,11 +2,24 @@ const covid19ImpactEstimator = (data) => ({
   data: {},
   impact: {
     currentlyInfected: data.reportedCases * 10,
-    infectionsByRequestedTime: data.reportedCases * 10 * 512
+    infectionsByRequestedTime: data.reportedCases * 10 * 512,
+    severeCasesByRequestedTime: 0.15 * data.reportedCases * 10 * 512,
+    hospitalBedsByRequestedTime: 23,
+    casesForICUByRequestedTime: 0.05 * data.reportedCases * 10 * 512,
+    casesForVentilatorsByRequestedTime: 0.02 * data.reportedCases * 10 * 512,
+    dollarInFlight:
+      this.infectionsByRequestedTime *
+      data.region.avgDailyIncomePopulation *
+      data.region.avgDailyIncomeInUSD *
+      30
   },
   severeImpact: {
     currentlyInfected: data.reportedCases * 50,
-    infectionsByRequestedTime: data.reportedCases * 10 * 512
+    infectionsByRequestedTime: data.reportedCases * 50 * 512,
+    severeCasesByRequestedTime: 0.15 * data.reportedCases * 50 * 512,
+    hospitalBedsByRequestedTime: 23,
+    casesForICUByRequestedTime: 0.05 * data.reportedCases * 50 * 512,
+    casesForVentilatorsByRequestedTime: 0.02 * data.reportedCases * 50 * 512
   }
 });
 

806164e895337881a0c12e8fa7c4ffcff0683d69
Malik <malikmukhtar63@gmail.com>
2020-04-08 12:52:44 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,13 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => ({
+  data: {},
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: data.reportedCases * 10 * 512
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: data.reportedCases * 10 * 512
+  }
+});
 
 export default covid19ImpactEstimator;

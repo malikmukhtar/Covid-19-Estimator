bad6bd0907076d77564ff4596cda1efb0a2e973f
Malik <malikmukhtar63@gmail.com>
2020-04-09 22:00:04 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,24 +1,24 @@
 const impactInfectionsByRequestedTime = (data) => {
-  if (data === 'days') {
+  if (data.periodType === 'days') {
     return Math.trunc(data.timeToElapse / 3);
   }
-  if (data === 'weeks') {
+  if (data.periodType === 'weeks') {
     return Math.trunc((data.timeToElapse * 7) / 3);
   }
-  if (data === 'months') {
+  if (data.periodType === 'months') {
     return Math.trunc((data.timeToElapse * 30) / 3);
   }
   return 0;
 };
 
 const severeInfectionsByRequestedTime = (data) => {
-  if (data === 'days') {
+  if (data.periodType === 'days') {
     return Math.trunc(data.timeToElapse / 3);
   }
-  if (data === 'weeks') {
+  if (data.periodType === 'weeks') {
     return Math.trunc((data.timeToElapse * 7) / 3);
   }
-  if (data === 'months') {
+  if (data.periodType === 'months') {
     return Math.trunc((data.timeToElapse * 30) / 3);
   }
   return 0;

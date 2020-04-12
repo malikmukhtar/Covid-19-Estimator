36c0d750a283f494897d1ea156fc292a1748c386
Malik <malikmukhtar63@gmail.com>
2020-04-12 13:12:44 +0100

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,5 @@
 // Normalizing Impact...
-const impactInfectionsByRequestedTime = (data) => {
+const infectionsByRequestedTime = (data) => {
   if (data.periodType === 'days') {
     return Math.trunc(data.timeToElapse / 3);
   }
@@ -12,15 +12,15 @@ const impactInfectionsByRequestedTime = (data) => {
   return 0;
 };
 // Normalizing Severe..
-const severeInfectionsByRequestedTime = (data) => {
+const infectionsByRequestedTimeFlight = (data) => {
   if (data.periodType === 'days') {
     return Math.trunc(data.timeToElapse / 3);
   }
   if (data.periodType === 'weeks') {
-    return Math.trunc((data.timeToElapse * 7) / 3);
+    return Math.trunc((data.timeToElapse) / 3);
   }
   if (data.periodType === 'months') {
-    return Math.trunc((data.timeToElapse * 30) / 3);
+    return Math.trunc((data.timeToElapse) / 3);
   }
   return 0;
 };
@@ -30,36 +30,36 @@ const covid19ImpactEstimator = (data) => ({
   impact: {
     currentlyInfected: data.reportedCases * 10,
     infectionsByRequestedTime: Math.trunc(
-      data.reportedCases * 10 * 2 ** impactInfectionsByRequestedTime(data)
+      data.reportedCases * 10 * 2 ** infectionsByRequestedTime(data)
     ),
     severeCasesByRequestedTime: Math.trunc(
       0.15
       * data.reportedCases
       * 10
-      * 2 ** impactInfectionsByRequestedTime(data)
+      * 2 ** infectionsByRequestedTime(data)
     ),
     hospitalBedsByRequestedTime: Math.trunc(
       0.35 * data.totalHospitalBeds
       - 0.15
       * data.reportedCases
       * 10
-      * 2 ** impactInfectionsByRequestedTime(data)
+      * 2 ** infectionsByRequestedTime(data)
     ),
     casesForICUByRequestedTime:
       0.05
       * data.reportedCases
       * 10
-      * 2 ** impactInfectionsByRequestedTime(data),
+      * 2 ** infectionsByRequestedTime(data),
     casesForVentilatorsByRequestedTime: Math.trunc(
       0.02
       * data.reportedCases
       * 10
-      * 2 ** impactInfectionsByRequestedTime(data)
+      * 2 ** infectionsByRequestedTime(data)
     ),
     dollarsInFlight: Math.trunc(
       (data.reportedCases
         * 10
-        * 2 ** impactInfectionsByRequestedTime(data)
+        * 2 ** infectionsByRequestedTimeFlight(data)
         * data.region.avgDailyIncomePopulation
         * data.region.avgDailyIncomeInUSD)
       / data.timeToElapse
@@ -68,37 +68,37 @@ const covid19ImpactEstimator = (data) => ({
   severeImpact: {
     currentlyInfected: data.reportedCases * 50,
     infectionsByRequestedTime: Math.trunc(
-      data.reportedCases * 50 * 2 ** severeInfectionsByRequestedTime(data)
+      data.reportedCases * 50 * 2 ** infectionsByRequestedTime(data)
     ),
     severeCasesByRequestedTime: Math.trunc(
       data.reportedCases
       * 0.15
       * 50
-      * 2 ** severeInfectionsByRequestedTime(data)
+      * 2 ** infectionsByRequestedTime(data)
     ),
     hospitalBedsByRequestedTime: Math.trunc(
       0.35 * data.totalHospitalBeds
       - 0.15
       * data.reportedCases
       * 50
-      * 2 ** severeInfectionsByRequestedTime(data)
+      * 2 ** infectionsByRequestedTime(data)
     ),
     casesForICUByRequestedTime: Math.trunc(
       0.05
       * data.reportedCases
       * 50
-      * 2 ** severeInfectionsByRequestedTime(data)
+      * 2 ** infectionsByRequestedTime(data)
     ),
     casesForVentilatorsByRequestedTime: Math.trunc(
       0.02
       * data.reportedCases
       * 50
-      * 2 ** severeInfectionsByRequestedTime(data)
+      * 2 ** infectionsByRequestedTime(data)
     ),
     dollarsInFlight: Math.trunc(
       (data.reportedCases
         * 50
-        * 2 ** severeInfectionsByRequestedTime(data)
+        * 2 ** infectionsByRequestedTimeFlight(data)
         * data.region.avgDailyIncomePopulation
         * data.region.avgDailyIncomeInUSD)
       / data.timeToElapse

ca1f8f18b447ae258cead1b89620db8737516e59
Malik <malikmukhtar63@gmail.com>
2020-04-12 11:43:03 +0100

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,26 @@
 // Normalizing Impact...
 const impactInfectionsByRequestedTime = (data) => {
   if (data.periodType === 'days') {
-    return Math.trunc(data.timeToElapse / 3);
+    return (data.timeToElapse / 3);
   }
   if (data.periodType === 'weeks') {
-    return Math.trunc((data.timeToElapse * 7) / 3);
+    return ((data.timeToElapse * 7) / 3);
   }
   if (data.periodType === 'months') {
-    return Math.trunc((data.timeToElapse * 30) / 3);
+    return ((data.timeToElapse * 30) / 3);
   }
   return 0;
 };
 // Normalizing Severe
 const severeInfectionsByRequestedTime = (data) => {
   if (data.periodType === 'days') {
-    return Math.trunc(data.timeToElapse / 3);
+    return (data.timeToElapse / 3);
   }
   if (data.periodType === 'weeks') {
-    return Math.trunc((data.timeToElapse * 7) / 3);
+    return ((data.timeToElapse * 7) / 3);
   }
   if (data.periodType === 'months') {
-    return Math.trunc((data.timeToElapse * 30) / 3);
+    return ((data.timeToElapse * 30) / 3);
   }
   return 0;
 };

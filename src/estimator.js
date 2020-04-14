62dd327915b8fb68e388d96ebf1dbcf655930a8c
Malik <malikmukhtar63@gmail.com>
2020-04-14 10:55:00 +0100

    challenge 4

--- src/estimator.js
+++ src/estimator.js
@@ -14,47 +14,62 @@ const convertToDays = (data) => {
   return 0;
 };
 
-// Estimator
+// Estimator..
 const covid19ImpactEstimator = (data) => {
   // Challenge 1
 
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
 
-  impact.infectionsByRequestedTime = (impact.currentlyInfected
-    * (2 ** Math.trunc((convertToDays(data) / 3))));
-  severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected
-    * (2 ** Math.trunc((convertToDays(data) / 3))));
+  impact.infectionsByRequestedTime =
+    impact.currentlyInfected * 2 ** Math.trunc(convertToDays(data) / 3);
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * 2 ** Math.trunc(convertToDays(data) / 3);
 
   // Challenge 2
 
-  impact.severeCasesByRequestedTime = Math.trunc(impact.infectionsByRequestedTime
-    * 0.15);
-  severeImpact.severeCasesByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime
-    * 0.15);
+  impact.severeCasesByRequestedTime = Math.trunc(
+    impact.infectionsByRequestedTime * 0.15
+  );
+  severeImpact.severeCasesByRequestedTime = Math.trunc(
+    severeImpact.infectionsByRequestedTime * 0.15
+  );
 
-  impact.hospitalBedsByRequestedTime = Math.trunc(((0.35 * data.totalHospitalBeds)
-    - impact.severeCasesByRequestedTime));
-  severeImpact.hospitalBedsByRequestedTime = Math.trunc(((0.35 * data.totalHospitalBeds)
-    - severeImpact.severeCasesByRequestedTime));
+  impact.hospitalBedsByRequestedTime = Math.trunc(
+    0.35 * data.totalHospitalBeds - impact.severeCasesByRequestedTime
+  );
+  severeImpact.hospitalBedsByRequestedTime = Math.trunc(
+    0.35 * data.totalHospitalBeds - severeImpact.severeCasesByRequestedTime
+  );
 
   // Challenge 3
 
-  impact.casesForICUByRequestedTime = Math.trunc(0.05 * impact.infectionsByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = Math.trunc(0.05
-    * severeImpact.infectionsByRequestedTime);
+  impact.casesForICUByRequestedTime = Math.trunc(
+    0.05 * impact.infectionsByRequestedTime
+  );
+  severeImpact.casesForICUByRequestedTime = Math.trunc(
+    0.05 * severeImpact.infectionsByRequestedTime
+  );
 
-  impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02
-    * impact.infectionsByRequestedTime);
-  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(0.02
-    * severeImpact.infectionsByRequestedTime);
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(
+    0.02 * impact.infectionsByRequestedTime
+  );
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(
+    0.02 * severeImpact.infectionsByRequestedTime
+  );
 
-  impact.dollarsInFlight = Math.trunc((impact.infectionsByRequestedTime
-    * data.region.avgDailyIncomePopulation
-    * data.region.avgDailyIncomeInUSD) / convertToDays(data));
-  severeImpact.dollarsInFlight = Math.trunc((severeImpact.infectionsByRequestedTime
-    * data.region.avgDailyIncomePopulation
-    * data.region.avgDailyIncomeInUSD) / convertToDays(data));
+  impact.dollarsInFlight = Math.trunc(
+    (impact.infectionsByRequestedTime *
+      data.region.avgDailyIncomePopulation *
+      data.region.avgDailyIncomeInUSD) /
+      convertToDays(data)
+  );
+  severeImpact.dollarsInFlight = Math.trunc(
+    (severeImpact.infectionsByRequestedTime *
+      data.region.avgDailyIncomePopulation *
+      data.region.avgDailyIncomeInUSD) /
+      convertToDays(data)
+  );
 
   return { data, impact, severeImpact };
 };
